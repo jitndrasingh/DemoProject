02b4b4a5cabffca9d27f7322769d45fa45452335
Kishan Kumar <64035167+kkjavatutorials@users.noreply.github.com>
2020-04-27 00:13:16 +0530

    doing multilplication
    doing multilplication

--- DemoProject/src/kk/com/Test.java
+++ DemoProject/src/kk/com/Test.java
@@ -9,9 +9,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello!!");
 		
-		int x = 10;
-		int y = 20;
-		System.out.println(x+y);
+		int i1 = 10;
+		int i2 = 20;
+		System.out.println(i1*i2);
 	}
 
 }
