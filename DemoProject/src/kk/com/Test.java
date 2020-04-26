cdac3e1441894bd1f96a5a13ed96b7d101d20731
kkjavatutorials <kkjavatutorials@gmail.com>
2020-04-27 00:07:06 +0530

    commit

--- DemoProject/src/kk/com/Test.java
+++ DemoProject/src/kk/com/Test.java
@@ -9,10 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello!!");
 		
-		int a = 10;
-		int b = 20;
+		int i1 = 10;
+		int i2 = 20;
 		
-		System.out.println(a+b);
+		System.out.println(i1+i2);
 	}
 
 }
